ff9d751b5f52bf89cf4177360f373ee388b64bf2
chan <ayechanhtunnaing@gmail.com>
2021-03-03 15:43:17 +0630

    pretty url

--- config/web.php
+++ config/web.php
@@ -48,12 +48,12 @@ $config = [
         ],
         'db' => $db,
 
-     /*   'urlManager' => [
+       'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
-        ], */
+        ],
 
     ],
     'params' => $params,
